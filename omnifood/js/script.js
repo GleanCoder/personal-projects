f6f538434d5bff14575005343f37b593b2a2e6fd
GleanCoder <adityakiranacharya0@gmail.com>
2023-10-09 14:37:57 +0530

    Omnifood is Optimized

--- omnifood/js/script.js
+++ omnifood/js/script.js
@@ -8,3 +8,52 @@ const headerEl = document.querySelector(".header");
 navButtonEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+//Smooth Scrolling
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    //close Mobile Nav on click
+    if (link.classList.contains("nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+//sticky Navbar
+
+const sectionHeroEl = document.querySelector(".hero-section");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-120px",
+  }
+);
+obs.observe(sectionHeroEl);
